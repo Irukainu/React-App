766fa41c287dc7b75e053875e23e7cdeab15832e
irukainu <irukainu20@gmail.com>
2023-01-14 22:22:53 +0700

    js

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src="https://www.google.com/search?q=kanji+png&tbm=isch&ved=2ahUKEwiW5pvfrMf8AhWqk9gFHc-SDDAQ2-cCegQIABAA&oq=kanji+png&gs_lcp=CgNpbWcQAzIFCAAQgAQyBQgAEIAEMgQIABAeMgYIABAFEB4yBggAEAUQHjIGCAAQBRAeMgYIABAFEB4yBggAEAUQHjIGCAAQBRAeMgYIABAFEB46BAgjECc6CAgAEIAEELEDOgcIIxDqAhAnOgQIABBDOgcIABCxAxBDOgsIABCABBCxAxCDAToKCAAQgAQQsQMQDToNCAAQgAQQsQMQgwEQDToHCAAQgAQQDVDMD1iXLGD0NmgBcAB4AoABywKIAZAWkgEHMC4yLjkuMZgBAKABAaoBC2d3cy13aXotaW1nsAEKwAEB&sclient=img&ei=qsbCY5bpHaqn4t4Pz6WygAM&bih=664&biw=1366#imgrc=N87ssenRFJQnOM" className="App-logo" alt="logo" />
+        <p>
+        <code>SWORD ART ONLINE</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://open.spotify.com/track/1ucnuV88gFTfR3BalmznDk?autoplay=true"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
